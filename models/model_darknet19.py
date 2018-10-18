ebb35085365bc7e8fe1fc15fdab087c7e2dd712b
DESKTOP-GMDVQ9G\BongHoe Koo <wantrnqhdghl@naver.com>
2018-10-18 18:06:23 +0900

    1. tensorflow-darknet-animal-train 추가 2. batch_norm 추가 3. dropout 삭제

--- models/model_darknet19.py
+++ models/model_darknet19.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-
+import tensorflow.contrib as contrib
 # 0 cat , 1 dog, 2 elephant, 3 giraffe, 4 horse
 
-class model_animal_v1:
+class model_darknet19:
 
     def __init__(self, sess, name, class_count, learning_rate):
         self.sess = sess
@@ -15,78 +15,195 @@ class model_animal_v1:
 
         with tf.variable_scope(self.name):
             # placeholder 100x100 = 10000
-            self.X = tf.placeholder(tf.float32, [None, 224 * 224 * 3], name='input')
+            self.X = tf.placeholder(tf.float32, [None, 100 * 100 * 3], name='input')
             print(self.X)
-            X_input = tf.reshape(self.X, [-1, 224, 224, 3])
+
+            self.keep_layer = tf.placeholder(tf.bool, name='phase')
+            print(self.keep_layer)
+
             self.Y = tf.placeholder(tf.float32, [None, self.class_count])
             print(self.Y)
-            self.keep_layer = tf.placeholder(tf.float32, name='dropout')
-            print(self.keep_layer)
+
+            X_input = tf.reshape(self.X, [-1, 100, 100, 3])
+            print(X_input)
 
             # Layer1
             W1 = tf.Variable(tf.random_normal([3, 3, 3, 32], stddev=0.01))
             Bias1 = tf.Variable(tf.constant(0.1, shape=[32]))
-            hidden_layer1 = tf.nn.relu(tf.nn.conv2d(X_input, W1, [1, 1, 1, 1], padding='SAME', name='hidden_layer1') + Bias1)
+            hidden_layer1 = tf.nn.conv2d(X_input, W1, [1, 1, 1, 1], padding='SAME', name='hidden_layer1') + Bias1
+            hidden_layer1 = contrib.layers.batch_norm(hidden_layer1, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer1 = tf.nn.relu(hidden_layer1)
             pool_layer1 = tf.nn.max_pool(hidden_layer1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-            drop_layer1 = tf.nn.dropout(pool_layer1, keep_prob=self.keep_layer)
+            print(pool_layer1)
+
 
             # Layer2
             W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
             Bias2 = tf.Variable(tf.constant(0.1, shape=[64]))
-            hidden_layer2 = tf.nn.relu(tf.nn.conv2d(drop_layer1, W2, [1, 1, 1, 1], padding='SAME', name='hidden_layer2') + Bias2)
+            hidden_layer2 = tf.nn.relu(tf.nn.conv2d(pool_layer1, W2, [1, 1, 1, 1], padding='SAME', name='hidden_layer2') + Bias2)
+            hidden_layer2 = contrib.layers.batch_norm(hidden_layer2, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer2 = tf.nn.relu(hidden_layer2)
             pool_layer2 = tf.nn.max_pool(hidden_layer2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-            drop_layer2 = tf.nn.dropout(pool_layer2, keep_prob=self.keep_layer)
+            print(pool_layer2)
 
             # Layer3
             W3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
             Bias3 = tf.Variable(tf.constant(0.1, shape=[128]))
-            hidden_layer3 = tf.nn.relu(tf.nn.conv2d(drop_layer2, W3, [1, 1, 1, 1], padding='SAME', name='hidden_layer3') + Bias3)
-            drop_layer3 = tf.nn.dropout(hidden_layer3, keep_prob=self.keep_layer)
+            hidden_layer3 = tf.nn.conv2d(pool_layer2, W3, [1, 1, 1, 1], padding='SAME', name='hidden_layer3') + Bias3
+            hidden_layer3 = contrib.layers.batch_norm(hidden_layer3, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer3 = tf.nn.relu(hidden_layer3)
+            print(hidden_layer3)
 
             # Layer4
-            W4 = tf.Variable(tf.random_normal([3, 3, 128, 64], stddev=0.01))
+            W4 = tf.Variable(tf.random_normal([1, 1, 128, 64], stddev=0.01))
             Bias4 = tf.Variable(tf.constant(0.1, shape=[64]))
-            hidden_layer4 = tf.nn.relu(tf.nn.conv2d(drop_layer3, W4, [1, 1, 1, 1], padding='SAME', name='hidden_layer4') + Bias4)
-            drop_layer4 = tf.nn.dropout(hidden_layer4, keep_prob=self.keep_layer)
+            hidden_layer4 = tf.nn.conv2d(hidden_layer3, W4, [1, 1, 1, 1], padding='SAME', name='hidden_layer4') + Bias4
+            hidden_layer4 = contrib.layers.batch_norm(hidden_layer4, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer4 = tf.nn.relu(hidden_layer4)
+            print(hidden_layer4)
 
             # Layer5
             W5 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
             Bias5 = tf.Variable(tf.constant(0.1, shape=[128]))
-            hidden_layer5 = tf.nn.relu(tf.nn.conv2d(drop_layer4, W5, [1, 1, 1, 1], padding='SAME', name='hidden_layer5') + Bias5)
+            hidden_layer5 = tf.nn.conv2d(hidden_layer4, W5, [1, 1, 1, 1], padding='SAME', name='hidden_layer5') + Bias5
+            hidden_layer5 = contrib.layers.batch_norm(hidden_layer5, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer5 = tf.nn.relu(hidden_layer5)
             pool_layer5 = tf.nn.max_pool(hidden_layer5, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-            drop_layer5 = tf.nn.dropout(pool_layer5, keep_prob=self.keep_layer)
+            print(pool_layer5)
 
             # Layer6
             W6 = tf.Variable(tf.random_normal([3, 3, 128, 256], stddev=0.01))
             Bias6 = tf.Variable(tf.constant(0.1, shape=[256]))
-            hidden_layer6 = tf.nn.relu(tf.nn.conv2d(drop_layer5, W6, [1, 1, 1, 1], padding='SAME', name='hidden_layer6') + Bias6)
-            pool_layer6 = tf.nn.max_pool(hidden_layer6, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-            drop_layer6 = tf.nn.dropout(pool_layer6, keep_prob=self.keep_layer)
+            hidden_layer6 = tf.nn.conv2d(pool_layer5, W6, [1, 1, 1, 1], padding='SAME', name='hidden_layer6') + Bias6
+            hidden_layer6 = contrib.layers.batch_norm(hidden_layer6, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer6 = tf.nn.relu(hidden_layer6)
+            print(hidden_layer6)
 
-            print(drop_layer6)
+            # Layer7
+            W7 = tf.Variable(tf.random_normal([1, 1, 256, 128], stddev=0.01))
+            Bias7 = tf.Variable(tf.constant(0.1, shape=[128]))
+            hidden_layer7 = tf.nn.conv2d(hidden_layer6, W7, [1, 1, 1, 1], padding='SAME', name='hidden_layer7') + Bias7
+            hidden_layer7 = contrib.layers.batch_norm(hidden_layer7, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer7 = tf.nn.relu(hidden_layer7)
+            print(hidden_layer7)
+
+            # Layer8
+            W8 = tf.Variable(tf.random_normal([3, 3, 128, 256], stddev=0.01))
+            Bias8 = tf.Variable(tf.constant(0.1, shape=[256]))
+            hidden_layer8 = tf.nn.conv2d(hidden_layer7, W8, [1, 1, 1, 1], padding='SAME', name='hidden_layer8') + Bias8
+            hidden_layer8 = contrib.layers.batch_norm(hidden_layer8, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer8 = tf.nn.relu(hidden_layer8)
+            pool_layer8 = tf.nn.max_pool(hidden_layer8, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+            print(pool_layer8)
+
+            # Layer9
+            W9 = tf.Variable(tf.random_normal([3, 3, 256, 512], stddev=0.01))
+            Bias9 = tf.Variable(tf.constant(0.1, shape=[512]))
+            hidden_layer9 = tf.nn.conv2d(pool_layer8, W9, [1, 1, 1, 1], padding='SAME', name='hidden_layer9') + Bias9
+            hidden_layer9 = contrib.layers.batch_norm(hidden_layer9, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer9 = tf.nn.relu(hidden_layer9)
+            print(hidden_layer9)
+
+            # Layer10
+            W10 = tf.Variable(tf.random_normal([1, 1, 512, 256], stddev=0.01))
+            Bias10 = tf.Variable(tf.constant(0.1, shape=[256]))
+            hidden_layer10 = tf.nn.conv2d(hidden_layer9, W10, [1, 1, 1, 1], padding='SAME', name='hidden_layer10') + Bias10
+            hidden_layer10 = contrib.layers.batch_norm(hidden_layer10, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer10 = tf.nn.relu(hidden_layer10)
+            print(hidden_layer10)
 
             # Layer11
-            flatten = tf.reshape(drop_layer6, [-1, 2 * 2 * 256])
-            full_connect_layer1 = tf.layers.dense(flatten, 1000, activation=tf.nn.relu)
-            full_connect_dropout1 = tf.nn.dropout(full_connect_layer1, keep_prob=self.keep_layer)
-            full_connect_layer2 = tf.layers.dense(full_connect_dropout1, 1000, activation=tf.nn.relu)
-            full_connect_dropout2 = tf.nn.dropout(full_connect_layer2, keep_prob=self.keep_layer)
-            full_connect_layer3 = tf.layers.dense(full_connect_dropout2, self.class_count)
+            W11 = tf.Variable(tf.random_normal([3, 3, 256, 512], stddev=0.01))
+            Bias11 = tf.Variable(tf.constant(0.1, shape=[512]))
+            hidden_layer11 = tf.nn.conv2d(hidden_layer10, W11, [1, 1, 1, 1], padding='SAME', name='hidden_layer11') + Bias11
+            hidden_layer11 = contrib.layers.batch_norm(hidden_layer11, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer11 = tf.nn.relu(hidden_layer11)
+            print(hidden_layer11)
 
-            self.output = tf.nn.softmax(full_connect_layer3, -1, 'output');
+            # Layer12
+            W12 = tf.Variable(tf.random_normal([1, 1, 512, 256], stddev=0.01))
+            Bias12 = tf.Variable(tf.constant(0.1, shape=[256]))
+            hidden_layer12 = tf.nn.conv2d(hidden_layer11, W12, [1, 1, 1, 1], padding='SAME', name='hidden_layer12') + Bias12
+            hidden_layer12 = contrib.layers.batch_norm(hidden_layer12, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer12 = tf.nn.relu(hidden_layer12)
+            print(hidden_layer12)
+
+            # Layer13
+            W13 = tf.Variable(tf.random_normal([3, 3, 256, 512], stddev=0.01))
+            Bias13 = tf.Variable(tf.constant(0.1, shape=[512]))
+            hidden_layer13 = tf.nn.conv2d(hidden_layer12, W13, [1, 1, 1, 1], padding='SAME', name='hidden_layer13') + Bias13
+            hidden_layer13 = contrib.layers.batch_norm(hidden_layer13, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer13 = tf.nn.relu(hidden_layer13)
+            pool_layer13 = tf.nn.max_pool(hidden_layer13, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+            print(pool_layer13)
+
+            # Layer14
+            W14 = tf.Variable(tf.random_normal([3, 3, 512, 1024], stddev=0.01))
+            Bias14 = tf.Variable(tf.constant(0.1, shape=[1024]))
+            hidden_layer14 = tf.nn.conv2d(pool_layer13, W14, [1, 1, 1, 1], padding='SAME', name='hidden_layer14') + Bias14
+            hidden_layer14 = contrib.layers.batch_norm(hidden_layer14, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer14 = tf.nn.relu(hidden_layer14)
+            print(hidden_layer14)
+
+            # Layer15
+            W15 = tf.Variable(tf.random_normal([1, 1, 1024, 512], stddev=0.01))
+            Bias15 = tf.Variable(tf.constant(0.1, shape=[512]))
+            hidden_layer15 = tf.nn.conv2d(hidden_layer14, W15, [1, 1, 1, 1], padding='SAME', name='hidden_layer15') + Bias15
+            hidden_layer15 = contrib.layers.batch_norm(hidden_layer15, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer15 = tf.nn.relu(hidden_layer15)
+            print(hidden_layer15)
+
+            # Layer16
+            W16 = tf.Variable(tf.random_normal([3, 3, 512, 1024], stddev=0.01))
+            Bias16 = tf.Variable(tf.constant(0.1, shape=[1024]))
+            hidden_layer16 = tf.nn.conv2d(hidden_layer15, W16, [1, 1, 1, 1], padding='SAME', name='hidden_layer16') + Bias16
+            hidden_layer16 = contrib.layers.batch_norm(hidden_layer16, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer16 = tf.nn.relu(hidden_layer16)
+            print(hidden_layer16)
+
+            # Layer17
+            W17 = tf.Variable(tf.random_normal([1, 1, 1024, 512], stddev=0.01))
+            Bias17 = tf.Variable(tf.constant(0.1, shape=[512]))
+            hidden_layer17 = tf.nn.conv2d(hidden_layer16, W17, [1, 1, 1, 1], padding='SAME', name='hidden_layer17') + Bias17
+            hidden_layer17 = contrib.layers.batch_norm(hidden_layer17, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer17 = tf.nn.relu(hidden_layer17)
+            print(hidden_layer17)
+
+            # Layer18
+            W18 = tf.Variable(tf.random_normal([3, 3, 512, 1024], stddev=0.01))
+            Bias18 = tf.Variable(tf.constant(0.1, shape=[1024]))
+            hidden_layer18 = tf.nn.conv2d(hidden_layer17, W18, [1, 1, 1, 1], padding='SAME', name='hidden_layer18') + Bias18
+            hidden_layer18 = contrib.layers.batch_norm(hidden_layer18, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer18 = tf.nn.relu(hidden_layer18)
+            print(hidden_layer18)
+
+            # Layer19
+            W19 = tf.Variable(tf.random_normal([1, 1, 1024, self.class_count], stddev=0.01))
+            Bias19 = tf.Variable(tf.constant(0.1, shape=[self.class_count]))
+            hidden_layer19 = tf.nn.conv2d(hidden_layer18, W19, [1, 1, 1, 1], padding='SAME', name='hidden_layer19') + Bias19
+            hidden_layer19 = contrib.layers.batch_norm(hidden_layer19, center=True, scale=True, is_training=self.keep_layer)
+            hidden_layer19 = tf.nn.relu(hidden_layer19)
+            print(hidden_layer19)
+
+
+            # Global Average Pooling
+            average_polling = tf.reduce_mean(hidden_layer19, axis=(1, 2), name="GAP")
+            print(average_polling)
+
+            self.output = tf.nn.softmax(average_polling, -1, 'output');
             print(self.output)
             correct_prediction = tf.equal(tf.argmax(self.output, 1), tf.argmax(self.Y, 1))
 
             # define cost/loss & optimizer
-            self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=full_connect_layer3, labels=self.Y))
+            self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=average_polling, labels=self.Y))
             self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
             self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    def predict(self, x_test, keep_prop=1.0):
+    def predict(self, x_test, keep_prop=False):
         return self.sess.run(self.output, feed_dict={self.X: x_test, self.keep_layer: keep_prop})
 
-    def get_accuracy(self, x_test, y_test, keep_prop=1.0):
+    def get_accuracy(self, x_test, y_test, keep_prop=False):
         return self.sess.run(self.accuracy, feed_dict={self.X: x_test, self.Y: y_test, self.keep_layer: keep_prop})
 
-    def train(self, x_data, y_data, keep_prop=0.8):
+    def train(self, x_data, y_data, keep_prop=True):
         return self.sess.run([self.cost, self.optimizer], feed_dict={self.X: x_data, self.Y: y_data, self.keep_layer: keep_prop})
---
+++ tensorflow-darknet-animal-train.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from util.datasetloader import datasetloader
+from util.datasetloader import pathtype
+from models.model_darknet19 import model_darknet19
+
+loader_train = datasetloader('D://DeepLearning//dataset//animal-train-v1', pathtype.absolute)
+loader_validation = datasetloader('D://DeepLearning//dataset//animal-validation-v1', pathtype.absolute)
+
+classCount = loader_validation.label_count()
+validationCount = loader_validation.sample_count()
+
+train_epoch = 500
+batch_size = 100
+sample_size = loader_train.sample_count()
+total_batch = int(sample_size / batch_size)
+target_accuracy = 0.90
+
+## cat 0, dog 1, elephant 2, giraffe 3, hourse 4
+sess = tf.Session()
+model1 = model_darknet19(sess=sess, name="animal-darknet19", class_count=classCount, learning_rate=0.001)
+sess.run(tf.global_variables_initializer())
+
+print('learning started')
+
+cost_graph = []
+accuracy_graph = []
+
+for epoch in range(train_epoch):
+    avg_cost = 0
+    accuracy_cost = 0
+    loader_train.clear()
+
+    for i in range(total_batch):
+        inputs_train, outputs_train = loader_train.load([100*100*3], 255, batch_size)
+        if inputs_train is None or outputs_train is None:
+            loader_train.clear()
+            break
+        c, _ = model1.train(inputs_train, outputs_train, True)
+        avg_cost += (c / total_batch)
+
+    inputs_validation, output_validation = loader_validation.load([100*100*3], 255, validationCount)
+    loader_validation.clear()
+    accuracy_value = model1.get_accuracy(inputs_validation, output_validation, False)
+    accuracy_graph.append(accuracy_value)
+    cost_graph.append(avg_cost)
+    print('Epoch : ', '%04d' %(epoch + 1), 'cost =',  '{:.9f}'.format(avg_cost), 'accuracy =', '{:.9f}'.format(accuracy_value))
+
+    if accuracy_value >= target_accuracy:
+        break
+
+saver = tf.train.Saver()
+saver.save(sess, './pretrained-models/animal-darknet19/animal-darknet19')
+
+plt.plot(cost_graph)
+plt.plot(accuracy_graph)
+plt.ylabel('cost, accuracy')
+plt.legend(['cost', 'accuracy'], loc='upper left')
+plt.savefig('./pretrained-models/animal-darknet19/animal-darknet19-graph.png')
+plt.show()
+
+print('Learning finished.')
